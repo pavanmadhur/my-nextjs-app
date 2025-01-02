fa6151aab7aa13bad16b89ab9ae7864596be18c8
pavanmadhur <pavanmadhurjune30@gmail.com>
2025-01-02 08:49:17 +0530

    NEw

--- src/app/admin/contacts/page.js
+++ src/app/admin/contacts/page.js
@@ -1,7 +1,5 @@
 "use client";
 import AdminContact from "@/components/AdminContact";
-import { LogOut } from "lucide-react";
-import { Heart } from "lucide-react";
 import { useRouter } from "next/navigation";
 export default function Page() {
   const router = useRouter();
