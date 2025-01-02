d89af1065577a8cf76aaf330cf829955b8ae1975
pavanmadhur <pavanmadhurjune30@gmail.com>
2025-01-02 08:56:45 +0530

    NEW

--- src/app/admin/contacts/page.js
+++ src/app/admin/contacts/page.js
@@ -1,11 +1,8 @@
 "use client";
 import AdminContact from "@/components/AdminContact";
-import { useRouter } from "next/navigation";
 export default function Page() {
-  const router = useRouter();
   return (
-    <>
-      
+    <> 
       <AdminContact></AdminContact>
     </>
   );
